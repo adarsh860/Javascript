1f42d010295625a646d2e895e29871d35f7ff6b6
Adarsh Pratap Singh <109974812+adarsh860@users.noreply.github.com>
2024-05-23 11:26:32 +0000

    variable

---
+++ 1_Basics/1_variables.js
@@ -0,0 +1,13 @@
+const userId = "123"
+let userName = "Adarsh Pratap"
+var useradd = "Delhi"
+userclass = "A";
+
+// userId = "12" // not allowed const changes
+ userName = "Adarsh"
+ useradd = "New Delhi"
+ userclass = "A+";
+ let userstate;
+
+console.log(userId)
+console.table([userId,userName,useradd,userclass,userstate])
