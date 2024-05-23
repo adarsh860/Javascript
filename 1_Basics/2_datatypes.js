138847c68d1916ff826fdb928cf253a256942e45
Adarsh Pratap Singh <109974812+adarsh860@users.noreply.github.com>
2024-05-23 12:08:17 +0000

    datatypes

---
+++ 1_Basics/2_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as new version
+// alert(1+2)  we are using nodejs not browser
+
+let a = "ashish"
+let age = 39;
+let logIn = false;
+
+// number = 2 to power 53
+// big int 
+// string =""
+// boolean=> True/ False
+// null = standalone value
+// undefined = value is not assigned
+// symbol = unique
+// object
+console.log(typeof "raj");
+console.log(typeof null);
+
