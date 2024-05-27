4b23c6a610af868575c0372063edf92e58adb4ba
Adarsh Pratap Singh <109974812+adarsh860@users.noreply.github.com>
2024-05-27 10:42:20 +0000

    conversion

---
+++ 1_Basics/3_conversion.js
@@ -0,0 +1,52 @@
+let score = "67a"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" = 33
+// "67a" = NaN
+// true = 1 , false = 0
+
+let is = 1 
+
+let booleanis = Boolean(is)
+// console.log(booleanis);
+ 
+// 1 => true , 0 => false
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+
+//********************************Operation*************************** */
+
+let value = 67
+
+let negValue = - value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 to power 2
+// console.log(2**3); // 2 to power 3
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "adarsh"
+let str2 =" pratap"
+// console.log(str1 + str2); // addition of string
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1+2+"2");
+
